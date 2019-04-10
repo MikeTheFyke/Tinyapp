0daa9965a9bc18614e5627e9b6a60215937fa66d
MikeTheFyke <mikefyke@hotmail.com>
2019-04-10 14:16:58 +0000

    Added a Second Route

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  "9sm5xK": "http://www.google.com"
 };
 
 app.get("/urls", (req, res) => {
@@ -27,10 +27,11 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
   res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
