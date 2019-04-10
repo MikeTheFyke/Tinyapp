f8b01c45982b1b7e941f8abc8cd5da0f07808c46
MikeTheFyke <mikefyke@hotmail.com>
2019-04-10 01:58:51 +0000

    Created a list of longURL and shortURL but not completed step

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/urls", (req, res) => {
@@ -24,6 +24,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
