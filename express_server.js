b736c3bc3bb6d26000725c62912b6e402ee4cf0c
vagrant <mikefyke@hotmail.com>
2019-04-11 23:12:21 +0000

    Login and Logout functioning

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 var express = require("express");
+const cookieParser = require('cookie-parser');
 var app = express();
 var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use (cookieParser());
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,7 +15,7 @@ var urlDatabase = {
 };
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
@@ -30,11 +32,22 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
+
+app.post("/login", (req, res) => {
+  res.cookie ('username', req.body.username);
+  res.redirect("/urls");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = {username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
@@ -43,11 +56,6 @@ app.get("/u/:shortURL", (req, res) => {
   console.log(longURL);
   res.redirect(longURL);
 });
-
-app.post("/login", (req, res) => {
-  const username = res.params.username;
-  res.render("_header");
-})
 
 app.post("/urls", (req, res) => {
   var shortURLLoader = generateRandomString();
